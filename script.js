99be54a5b0c4c5995947c5dbc71d980d767c3105
melaniec207 <carrillomelanie54@gmail.com>
2025-06-20 17:43:44 +0000

    color box and text

---
+++ script.js
@@ -0,0 +1,10 @@
+function setup() //called once when the program starts 
+{
+    createCanvas(400,400);
+    background(220);
+}
+
+function draw()//This runs in a loop to to pdate the display 
+{
+
+}
